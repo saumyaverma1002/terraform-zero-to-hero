76f290056268d2b216c19334b44285524ac032f9
saumyaverma1002 <38492961+saumyaverma1002@users.noreply.github.com>
2024-08-26 23:52:00 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1 # change this
+    bucket         = "my-ews-baket1" # change this
     key            = "saumya/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
