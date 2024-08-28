59161d42fe447d7b4afda5eadd01e49ac43151fe
saumyaverma1002 <38492961+saumyaverma1002@users.noreply.github.com>
2024-08-28 13:22:53 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+/*resource "aws_instance" "abhishek" {
   instance_type = "t2.micro"
   ami = "ami-0261755bbcb8c4a84" # change this
   subnet_id = "subnet-0218ce1671129f988" # change this
-}
+}*/
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "my-ews-baket150" # change this
