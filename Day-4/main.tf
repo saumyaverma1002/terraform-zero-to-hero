54c31e685ebf688bc45f7aaf5d1b30c1ab00187e
saumyaverma1002 <38492961+saumyaverma1002@users.noreply.github.com>
2024-08-28 11:35:53 +0530

    Update main.tf (subnet id and amiid)

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "abhishek" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-0261755bbcb8c4a84" # change this
+  subnet_id = "subnet-0218ce1671129f988" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
